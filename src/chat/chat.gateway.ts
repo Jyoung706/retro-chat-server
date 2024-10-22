70af903f9a41bd81da026c5c78504199c5e8ec35
jYoung706 <wnsdud9476@gmail.com>
2024-10-22 19:50:32 +0900

    [Add] chat gateway

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,38 +1,23 @@
 import {
   WebSocketGateway,
+  OnGatewayConnection,
   SubscribeMessage,
   MessageBody,
 } from '@nestjs/websockets';
 import { ChatService } from './chat.service';
-import { CreateChatDto } from './dto/create-chat.dto';
-import { UpdateChatDto } from './dto/update-chat.dto';
+import { Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class ChatGateway {
+@WebSocketGateway({
+  namespace: 'chats',
+})
+export class ChatGateway implements OnGatewayConnection {
   constructor(private readonly chatService: ChatService) {}
-
-  @SubscribeMessage('createChat')
-  create(@MessageBody() createChatDto: CreateChatDto) {
-    return this.chatService.create(createChatDto);
+  handleConnection(socket: Socket) {
+    console.log(`on connect called : ${socket.id}`);
   }
 
-  @SubscribeMessage('findAllChat')
-  findAll() {
-    return this.chatService.findAll();
-  }
-
-  @SubscribeMessage('findOneChat')
-  findOne(@MessageBody() id: number) {
-    return this.chatService.findOne(id);
-  }
-
-  @SubscribeMessage('updateChat')
-  update(@MessageBody() updateChatDto: UpdateChatDto) {
-    return this.chatService.update(updateChatDto.id, updateChatDto);
-  }
-
-  @SubscribeMessage('removeChat')
-  remove(@MessageBody() id: number) {
-    return this.chatService.remove(id);
+  @SubscribeMessage('send_message')
+  sendMessage(@MessageBody() data: string) {
+    console.log(data);
   }
 }
