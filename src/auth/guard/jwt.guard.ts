4219a733ceaf924ec18db543f5781b37d9ef351a
jYoung706 <wnsdud9476@gmail.com>
2025-01-12 18:53:43 +0900

    [Add] public 데코레이터 추가

--- src/auth/guard/jwt.guard.ts
+++ src/auth/guard/jwt.guard.ts
@@ -3,11 +3,17 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { JsonWebTokenError, TokenExpiredError } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
+import { IS_PUBLIC_KEY } from 'src/common/decorators/public.decorator';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
   handleRequest<TUser = any>(
     err: any,
     user: any,
@@ -15,6 +21,15 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     context: ExecutionContext,
     status?: any,
   ): TUser {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true as any;
+    }
+
     if (info instanceof TokenExpiredError) {
       throw new UnauthorizedException('Token has expired');
     }
